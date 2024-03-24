fd9f0e85b8e3d333303460f91d26d7bf8f0cf08c
nasifay <nasifayc11@gmail.com>
2024-03-24 08:31:09 +0000

    Firebase Settup Configuration

--- afronex_shop_app/lib/main.dart
+++ afronex_shop_app/lib/main.dart
@@ -1,15 +1,24 @@
-
+import 'package:afronex_shop_app/firebase_options.dart';
 import 'package:afronex_shop_app/screens/landing.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  } catch (e) {
+    print('Firebase Intialization Error: $e');
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,4 +32,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
