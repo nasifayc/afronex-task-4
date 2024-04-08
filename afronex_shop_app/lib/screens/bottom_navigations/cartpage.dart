2ebc03ade603a9a701f9966f2b5b3612440d3e4d
nasifay <nasifayc11@gmail.com>
2024-04-08 02:26:40 +0100

    cart page modified

--- afronex_shop_app/lib/screens/bottom_navigations/cartpage.dart
+++ afronex_shop_app/lib/screens/bottom_navigations/cartpage.dart
@@ -11,114 +11,110 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Cart'),
-            centerTitle: true,
-          ),
-          body: GetBuilder<CartController>(builder: (_) {
-            final cartItems = _cartController.cartItems;
-            if (cartItems.isEmpty) {
-              return const Center(
-                child: Text('No Items Add'),
-              );
-            } else {
-              return Padding(
-                  padding: EdgeInsets.all(Get.width * 0.025),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: Get.height * 0.45,
-                        width: double.infinity,
-                        child: ListView.builder(
-                          itemCount: cartItems.length,
-                          itemBuilder: (context, index) {
-                            return Column(
-                              children: [
-                                CartCard(item: cartItems[index]),
-                                const SizedBox(
-                                  height: 15,
-                                )
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 10),
-                        decoration: const BoxDecoration(
-                          color: Color.fromARGB(255, 246, 243, 243),
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: AppBar(
+        title: const Text('Cart'),
+        centerTitle: true,
+      ),
+      body: GetBuilder<CartController>(builder: (_) {
+        final cartItems = _cartController.cartItems;
+        if (cartItems.isEmpty) {
+          return const Center(
+            child: Text('No Items Add'),
+          );
+        } else {
+          return Padding(
+              padding: EdgeInsets.all(Get.width * 0.025),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: Get.height * 0.45,
+                    width: double.infinity,
+                    child: ListView.builder(
+                      itemCount: cartItems.length,
+                      itemBuilder: (context, index) {
+                        return Column(
                           children: [
-                            const StyledText(
-                              title: 'Order Summary',
-                              isBold: true,
-                              fontSize: 18,
-                              color: Colors.black,
-                            ),
-                            Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
+                            CartCard(item: cartItems[index]),
+                            const SizedBox(
+                              height: 15,
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 246, 243, 243),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const StyledText(
+                          title: 'Order Summary',
+                          isBold: true,
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Column(
+                              children: [
+                                Row(
                                   children: [
-                                    Row(
-                                      children: [
-                                        const StyledText(
-                                            title: 'Items: ',
-                                            isBold: false,
-                                            fontSize: 18,
-                                            color: Colors.grey),
-                                        const Spacer(),
-                                        StyledText(
-                                          title: cartItems.length.toString(),
-                                          isBold: true,
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        )
-                                      ],
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    Row(
-                                      children: [
-                                        const StyledText(
-                                            title: 'Total: ',
-                                            isBold: false,
-                                            fontSize: 18,
-                                            color: Colors.grey),
-                                        const Spacer(),
-                                        StyledText(
-                                          title: _cartController.totalPrice
-                                              .toString(),
-                                          isBold: true,
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        )
-                                      ],
+                                    const StyledText(
+                                        title: 'Items: ',
+                                        isBold: false,
+                                        fontSize: 18,
+                                        color: Colors.grey),
+                                    const Spacer(),
+                                    StyledText(
+                                      title: cartItems.length.toString(),
+                                      isBold: true,
+                                      fontSize: 18,
+                                      color: Colors.black,
                                     )
                                   ],
-                                ))
-                          ],
-                        ),
-                      )
-                    ],
-                  ));
-            }
-          }),
-        ),
-      ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  children: [
+                                    const StyledText(
+                                        title: 'Total: ',
+                                        isBold: false,
+                                        fontSize: 18,
+                                        color: Colors.grey),
+                                    const Spacer(),
+                                    StyledText(
+                                      title:
+                                          _cartController.totalPrice.toString(),
+                                      isBold: true,
+                                      fontSize: 18,
+                                      color: Colors.black,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ))
+                      ],
+                    ),
+                  )
+                ],
+              ));
+        }
+      }),
     );
   }
 }
